b755c10bf7d0677dc2bb9092c74582d9813e4bf5
Descartes Tuyishime <tuyishime.descartes@outlook.com>
2026-03-09 04:46:51 -0500

    Add src/api/router.py: API router registration

---
+++ src/api/router.py
@@ -0,0 +1,19 @@
+"""API route aggregation."""
+
+from fastapi import APIRouter
+
+from src.api.admin import router as admin_router
+from src.api.agencies import router as agencies_router
+from src.api.auth import router as auth_router
+from src.api.invitations import router as invitations_router
+from src.api.password import router as password_router
+from src.api.profile import router as profile_router
+
+api_router = APIRouter(prefix="/api/v1")
+
+api_router.include_router(auth_router)
+api_router.include_router(invitations_router)
+api_router.include_router(profile_router)
+api_router.include_router(password_router)
+api_router.include_router(agencies_router)
+api_router.include_router(admin_router)
