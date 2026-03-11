dc56b2534c2b5d6e7cdfdf5ef9fcc9f054b860a0
Descartes Tuyishime <tuyishime.descartes@outlook.com>
2026-03-11 10:34:17 -0500

    feat: register messaging and notification routes

--- src/api/router.py
+++ src/api/router.py
@@ -6,6 +6,8 @@ from src.api.admin import router as admin_router
 from src.api.agencies import router as agencies_router
 from src.api.auth import router as auth_router
 from src.api.invitations import router as invitations_router
+from src.api.messaging import router as messaging_router
+from src.api.notifications import router as notifications_router
 from src.api.password import router as password_router
 from src.api.profile import router as profile_router
 
@@ -17,3 +19,5 @@ api_router.include_router(profile_router)
 api_router.include_router(password_router)
 api_router.include_router(agencies_router)
 api_router.include_router(admin_router)
+api_router.include_router(messaging_router)
+api_router.include_router(notifications_router)
